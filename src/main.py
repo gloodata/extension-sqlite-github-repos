d67e134dd8cc5bd64a6261018b54c9f4e1e8c588
Mariano Guerra <mariano@marianoguerra.org>
2025-04-29 14:26:49 +0200

    update deps

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-import os
 from toolbox import tb
 
-host = os.environ.get("EXTENSION_HOST", "localhost")
-port = int(os.environ.get("EXTENSION_PORT", "9876"))
-
-tb.serve(host=host, port=port)
+tb.serve_from_env_or(default_port=9876)
